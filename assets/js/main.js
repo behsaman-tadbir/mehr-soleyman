d6b260a07af03276855d8f23b38bc9d1c41d0575
behsaman-tadbir <dadashi.behsaman@gmail.com>
2025-12-23 11:53:02 +0330

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,3 +3,12 @@ function toggleMenu() {
     .getElementById('mobileMenu')
     .classList.toggle('show');
 }
+// ===== Auto Slider =====
+const slides = document.querySelectorAll('.slide');
+let currentSlide = 0;
+
+setInterval(() => {
+  slides[currentSlide].classList.remove('active');
+  currentSlide = (currentSlide + 1) % slides.length;
+  slides[currentSlide].classList.add('active');
+}, 3000);
